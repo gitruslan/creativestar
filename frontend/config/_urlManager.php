d27a34b8b840353dd1baa73a33adfa07e5b4402d
ruslan <ryslanfree@yandex.ru>
2015-06-14 23:24:18 +0300

    #add route for article & static pages

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -5,12 +5,14 @@ return [
     'showScriptName'=>false,
     'rules'=> [
         // Pages
-        ['pattern'=>'page/<slug>', 'route'=>'page/view'],
+        //['pattern'=>'page/<slug>', 'route'=>'page/view'],
+        ['pattern'=>'<slug:(about)>','route'=>'page/view'],
 
         // Articles
         ['pattern'=>'article/index', 'route'=>'article/index'],
         ['pattern'=>'article/attachment-download', 'route'=>'article/attachment-download'],
-        ['pattern'=>'article/<slug>', 'route'=>'article/view'],
+        ['pattern'=>'<category:(news|blog)>', 'route'=>'article/category-view'],
+        ['pattern'=>'<category:(news|blog)>/<slug>', 'route'=>'article/view'],
 
         // Api
         ['class' => 'yii\rest\UrlRule', 'controller' => 'api/v1/article', 'only' => ['index', 'view', 'options']],
