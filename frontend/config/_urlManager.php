0171ad5fdd335ac067422e3bf1815db61209e0e5
Labuta Ruslan <labuta@ufins.com>
2015-06-18 19:19:54 +0300

    add multy lang logic

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -3,6 +3,7 @@ return [
     'class'=>'yii\web\UrlManager',
     'enablePrettyUrl'=>true,
     'showScriptName'=>false,
+    'class'=>'frontend\components\LangUrlManager',
     'rules'=> [
         // Pages
         //['pattern'=>'page/<slug>', 'route'=>'page/view'],
