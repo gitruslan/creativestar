05c624e9cf4b9c98c07f59d352f5dcf11644da9b
ruslan <ryslanfree@yandex.ru>
2015-08-13 22:42:36 +0300

    add view logic for articles in admin

--- backend/web/js/app.js
+++ backend/web/js/app.js
@@ -10,4 +10,11 @@ $(function() {
         zIndex: 999999
     }).disableSelection();
     $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+    $("#article-category_id").change(function(){
+        var currentCat = '_'+$(":selected",this).html().toLowerCase();
+        var articleView = $("#article-view").val();
+        if(currentCat != articleView){
+            $("#article-view").val(currentCat);
+        }
+    });
 })
