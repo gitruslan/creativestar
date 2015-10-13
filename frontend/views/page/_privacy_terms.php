813662a2f23780ed55860f126d053461c1ea637b
ruslan <ryslanfree@yandex.ru>
2015-10-14 00:22:26 +0300

    add new view for static page

---
+++ frontend/views/page/_privacy_terms.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruslan
+ * Date: 10/13/15
+ * Time: 11:26 PM
+ * @var $this \yii\web\View
+ * @var $model \common\models\Page
+ */
+$this->title = $model->title;
+?>
+<div class="aboutus-right-img"></div>
+<div class="aboutus-left-img"></div>
+<div class="aboutus-blick-img"></div>
+<div class="privacy-terms-wrapper">
+    <div class="body-text">
+     <?php echo $model->body ?>
+    </div>
+</div>
